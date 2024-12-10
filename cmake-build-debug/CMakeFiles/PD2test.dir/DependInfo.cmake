c698a138bf70b9540446e5dc026014e2ace1e61f
Bartion1 <u59_bardeu_waw@technischools.com>
2024-12-10 18:49:25 +0100

    Almost done

---
+++ cmake-build-debug/CMakeFiles/PD2test.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/barto/OneDrive/Pulpit/Code/C/PD2/Node.c" "CMakeFiles/PD2test.dir/Node.c.obj" "gcc" "CMakeFiles/PD2test.dir/Node.c.obj.d"
+  "C:/Users/barto/OneDrive/Pulpit/Code/C/PD2/Vector.c" "CMakeFiles/PD2test.dir/Vector.c.obj" "gcc" "CMakeFiles/PD2test.dir/Vector.c.obj.d"
+  "C:/Users/barto/OneDrive/Pulpit/Code/C/PD2/main.c" "CMakeFiles/PD2test.dir/main.c.obj" "gcc" "CMakeFiles/PD2test.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
